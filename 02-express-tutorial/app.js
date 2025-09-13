4aecb737b40c1fc4bda20881cf51a74c8d63427b
john chege <joniemwash2@gmail.com>
2025-09-13 10:38:05 +0300

    app

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,6 +1,9 @@
-const http =require('http')
-const server=http.createServer((req, res)=>{
-    console.log('server is connected')
-})
+const http = require('http');
+const server = http.createServer((req, res) => {
+    console.log('server is connected');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>this is hoe page</h1>') // Set status and headers
+    res.end();
+});
 
-server.listen(5000)
+server.listen(5000);
