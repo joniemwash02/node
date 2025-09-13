e8bcc6e58ee334cfd4c4eaffb9fa1529364220ca
john chege <joniemwash2@gmail.com>
2025-09-13 10:56:23 +0300

    if elsee

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,9 +1,21 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-    console.log('server is connected');
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<h1>this is hoe page</h1>') // Set status and headers
-    res.end();
+    const url= req.url
+    if(url==='/'){
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<h1>this is home page</h1>') // Set status and headers
+        res.end();
+    }else if(url==='/about'){
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<h1>this is about page</h1>') // Set status and headers
+        res.end();
+
+    }else{
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.write('<h1>page not found</h1>') // Set status and headers
+        res.end();
+    }
+    
 });
 
 server.listen(5000);
